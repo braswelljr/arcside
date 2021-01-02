8931a7469bfbd5472a6e962e4dbbbaa904799aa4
braswelljr <braswellkenneth7@gmail.com>
2021-01-02 00:02:57 +0000

    Public dir update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,18 @@
 import React from "react";
+import router from "../router/index";
 
 const Navbar = ({ appname }) => {
   return (
-    <div>
-      <div className="">{appname}</div>
-    </div>
+    <nav className="navbar">
+      <div className="navbar-title">{appname}</div>
+      <div className="navbar-menu">
+        {router.map((view, i) => (
+          <div className="navbar-menu" key={i}>
+            {view.name}
+          </div>
+        ))}
+      </div>
+    </nav>
   );
 };
 
